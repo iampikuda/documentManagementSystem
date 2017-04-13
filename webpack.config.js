0ba3549fffbbc86e3f0bdd0df1b728e4bff96d60
andela-opikuda <oluwadamisi.pikuda@andela.com>
2017-04-13 10:35:57 +0100

    add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,56 @@
+// module.exports = {
+//   context: `${__dirname}/app`,
+
+//   entry: './js/app.js',
+
+//   output: {
+//     filename: 'bundle.js',
+//     path: `${__dirname}/app`,
+//   }
+// };
+
+const webpack = require('webpack');
+const path = require('path');
+
 module.exports = {
-  context: `${__dirname}/app`,
-
-  entry: './js/app.js',
-
+  entry: [
+    './client/src/index'
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          presets: ['react']
+        }
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          presets: ['react']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', 'jsx']
+  },
   output: {
-    filename: 'bundle.js',
-    path: `${__dirname}/app`,
-  }
+    path: path.join(__dirname, 'client/dist/'),
+    publicPath: '/app/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './client/dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
 };
