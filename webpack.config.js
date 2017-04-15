4db4eb29c6663a37bbc322b5bd8a7afdecc20ee0
andela-opikuda <oluwadamisi.pikuda@andela.com>
2017-04-15 13:00:44 +0100

    reverted to default

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,56 @@
-// module.exports = {
-//   context: `${__dirname}/app`,
-
-//   entry: './js/app.js',
-
-//   output: {
-//     filename: 'bundle.js',
-//     path: `${__dirname}/app`,
-//   }
-// };
-
-const webpack = require('webpack');
-const path = require('path');
-
 module.exports = {
-  entry: [
-    './client/src/index'
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.js?$/,
-        loader: 'babel',
-        exclude: /node_modules/,
-        query: {
-          presets: ['react']
-        }
-      },
-      {
-        test: /\.jsx$/,
-        loader: 'babel',
-        exclude: /node_modules/,
-        query: {
-          presets: ['react']
-        }
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['', '.js', 'jsx']
-  },
+  context: `${__dirname}/app`,
+
+  entry: './js/app.js',
+
   output: {
-    path: path.join(__dirname, 'client/dist/'),
-    publicPath: '/app/',
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: './client/dist',
-    hot: true
-  },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ]
+    filename: 'bundle.js',
+    path: `${__dirname}/app`,
+  }
 };
+
+// const webpack = require('webpack');
+// const path = require('path');
+
+// module.exports = {
+//   entry: [
+//     './client/src/index'
+//   ],
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.js?$/,
+//         loader: 'babel',
+//         exclude: /node_modules/,
+//         query: {
+//           presets: ['react']
+//         }
+//       },
+//       {
+//         test: /\.jsx$/,
+//         loader: 'babel',
+//         exclude: /node_modules/,
+//         query: {
+//           presets: ['react']
+//         }
+//       }
+//     ]
+//   },
+//   resolve: {
+//     extensions: ['', '.js', 'jsx']
+//   },
+//   output: {
+//     path: path.join(__dirname, 'client/dist/'),
+//     publicPath: '/app/',
+//     filename: 'bundle.js'
+//   },
+//   devServer: {
+//     contentBase: './client/dist',
+//     hot: true
+//   },
+//   plugins: [
+//     new webpack.optimize.OccurenceOrderPlugin(),
+//     new webpack.HotModuleReplacementPlugin(),
+//     new webpack.NoErrorsPlugin()
+//   ]
+// };
