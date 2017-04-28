058f4ffb2d421aed38824adf88c6d6f7cb3ef7a0
andela-opikuda <oluwadamisi.pikuda@andela.com>
2017-04-28 15:46:01 +0100

    add role and user functionality to dashboard

--- client/src/components/dashboard/Index.component.jsx
+++ client/src/components/dashboard/Index.component.jsx
@@ -8,6 +8,7 @@ import UserDashboard from './user.component.jsx';
 import { bindActionCreators } from 'redux';
 import * as docActions from '../../actions/documentManagement/readDocument.js';
 import * as userActions from '../../actions/userManagement/getUsers.js';
+import * as roleActions from '../../actions/userManagement/getRoles.js';
 
 class Dashboard extends Component {
   constructor(props) {
@@ -23,28 +24,34 @@ class Dashboard extends Component {
     const userId = this.state.authUser.userId || null
     this.props.actionsDoc.viewUserDocuments(userId);
     this.props.actionsUser.viewUsers(userId);
-    console.log('***', this.props);
+    this.props.actionsRole.viewRoles(userId);
   }
 
   render() {
     const roleId = this.state.authUser.roleId || null
     return (roleId === this.state.AdminRoleId) ?
-      <AdminDashboard documents={this.props.documents} users= {this.props.users}/> :
-      <UserDashboard documents={this.props.documents} />
+      <div>
+        <AdminDashboard documents={this.props.documents} users={this.props.users} roles={this.props.roles}/>
+      </div> :
+      <div>
+        <UserDashboard documents={this.props.documents} />
+      </div>
   }
 }
 
 const mapStoreToProps = (state) => {
   return {
     documents: state.documentReducer,
-    users: state.userReducer
+    users: state.userReducer,
+    roles: state.roleReducer
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     actionsDoc: bindActionCreators(docActions, dispatch),
-    actionsUser: bindActionCreators(userActions, dispatch)
+    actionsUser: bindActionCreators(userActions, dispatch),
+    actionsRole: bindActionCreators(roleActions, dispatch)
   }
 }
 
