679e671b9a8a12451b360d458ed3b6a3ff837ee7
andela-opikuda <oluwadamisi.pikuda@andela.com>
2017-04-28 16:01:11 +0100

    create role view component

---
+++ client/src/components/containers/admin/rolesView.component.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { browserHistory, Link } from 'react-router';
+import jwtDecode from 'jwt-decode';
+
+const SingleRoles = (roles, index) => {
+  return (
+    <tr className="hoverable" key={index}>
+      <td>{roles.id}</td>
+      <td>{roles.title}</td>
+      <td>{(roles.createdAt).slice(0, 10)}</td>
+      <td>{(roles.updatedAt).slice(0, 10)}</td>
+      {/*<td><a className="modal-trigger green-text" href="#modal1" onClick={() => { props.setEditroles(roles); }}><i className="material-icons">edit</i></a></td>
+      <td><a className="red-text" href="#" onClick={() => { props.setDeleteroles(roles.id); }} > <i className="material-icons">delete</i></a></td>*/}
+    </tr >
+  );
+}
+let rolesList = [];
+const RoleView = (props) => {
+   console.log('-=-=-=-=-=-=-', props);
+  if (props.roles.roles !== undefined) {
+    rolesList = props.roles.roles.data.roles;
+    if (rolesList === undefined) {
+      rolesList = props.roles.roles.data.roles;
+    }
+    console.log('-=-=-=-=-=-=-', rolesList);
+  }
+  return (
+    <div>
+      <table className="bordered  responsive">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Created date</th>
+            <th>Updated date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rolesList.map(SingleRoles)}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default RoleView;
