5a8a113ec273c24f163d0742021316dfa59dd6b5
andela-opikuda <oluwadamisi.pikuda@andela.com>
2017-04-28 15:45:10 +0100

    add role and user reducer to index

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,10 +3,12 @@ import loginReducer from './loginReducer';
 import signupReducer from './signupReducer';
 import documentReducer from './documentReducer';
 import userReducer from './userReducer';
+import roleReducer from './roleReducer';
 
 export default combineReducers({
   loginReducer,
   signupReducer,
   documentReducer,
-  userReducer
+  userReducer,
+  roleReducer
 });
