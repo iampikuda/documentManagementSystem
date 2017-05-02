a7a3b9f3723ad282d0904329995852804163b16a
andela-opikuda <oluwadamisi.pikuda@andela.com>
2017-05-02 10:56:25 +0100

    view doc modal modify truncate content in list view show all html in text format

--- client/src/components/dashboard/userDashboard/allDocs.component.jsx
+++ client/src/components/dashboard/userDashboard/allDocs.component.jsx
@@ -3,42 +3,37 @@ import { browserHistory, Link } from 'react-router';
 import jwtDecode from 'jwt-decode';
 
 
-const SingleDocument = (document, index) => {
-  // console.log('ahsdahdas', document, index);
-  return (
-    <tr className="hoverable" key={index}>
-      <td>{document.title}</td>
-      <td>{document.access}</td>
-      <td>{document.content}</td>
-      <td>{`${document.User.lastName} ${document.User.firstName}`}</td>
-      <td>{(document.createdAt).slice(0, 10)}</td>
-      <td>{(document.updatedAt).slice(0, 10)}</td>
-      {/*<td><a className="modal-trigger green-text" href="#modalEdit" onClick={() => { props.setEditDocument(document); }}><i className="material-icons">edit</i></a></td>
-    <td><a className="red-text" href="#" onClick={() => { props.setDeleteDocument(document.id); }} > <i className="material-icons">delete</i></a></td>*/}
-    </tr >
-  );
-}
-let firstName;
-
+let props;
 const UserDocs = (props) => {
+  const SingleDocument = (document, index) => {
+    console.log('ahsdahdas', document, index, this, props);
+    return (
+      <tr className="hoverable" key={index} >
+        <td>{document.title}</td>
+        <td>{document.access}</td>
+        <td className="truncate"><a href="#modalView" dangerouslySetInnerHTML={{ __html: document.content}} onClick={() => { props.setViewDocument(document); }} /></td>
+        <td>{`${document.User.lastName} ${document.User.firstName}`}</td>
+        <td>{(document.createdAt).slice(0, 10)}</td>
+        <td>{(document.updatedAt).slice(0, 10)}</td>
+        {/*</a>*/}
+        {/*<td><a className="modal-trigger green-text" href="#modalEdit" onClick={() => { props.setEditDocument(document); }}><i className="material-icons">edit</i></a></td>
+      <td><a className="red-text" href="#" onClick={() => { props.setDeleteDocument(document.id); }} > <i className="material-icons">delete</i></a></td>*/}
+      </tr >
+    );
+  }
+  console.log('10101010101', props);
   let documentList = [];
   const token = window.localStorage.getItem('token');
-  if (token) {
-    firstName = jwtDecode(token).firstName;
-  }
-  // console.log('+++++------', props);
   if (props.document.document !== undefined) {
     let docs = props.document.document.data.document;
     if (docs === undefined) {
       docs = props.document.document.data.documents;
     }
     documentList = docs;
-    // documentList = docs.map(SingleDocument)
-    // console.log(documentList, "jbkjjbbjbj")
   }
   return (
     <div>
-      <table className="bordered  responsive">
+      <table className="bordered centered responsive">
         <thead>
           <tr>
             <th>Title</th>
