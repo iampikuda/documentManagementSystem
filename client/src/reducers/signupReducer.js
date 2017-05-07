e853b614d1669e198ca786d9a3ad96da34fbb0f5
andela-opikuda <oluwadamisi.pikuda@andela.com>
2017-05-07 05:59:50 +0100

    reformat reducer

--- client/src/reducers/signupReducer.js
+++ client/src/reducers/signupReducer.js
@@ -3,9 +3,16 @@ import * as actionTypes from '../actions/actionTypes';
 export default (state = {}, action) => {
   switch (action.type) {
     case actionTypes.LOGIN_SUCCESSFUL:
-      return { ...state, user: Object.assign({}, action.user) };
+      return {
+        ...state,
+        user: Object.assign({}, action.user)
+      };
     case actionTypes.SIGNUP_FAILED:
-      return { ...state, error: action.message, success: null };
+      return {
+        ...state,
+        error: action.message,
+        success: null
+      };
     default:
       return state;
   }
