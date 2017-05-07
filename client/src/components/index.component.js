3be563e2732d5a51fd40d9e49a2c458dedd8cef0
andela-opikuda <oluwadamisi.pikuda@andela.com>
2017-05-07 06:00:50 +0100

    remove unused import

--- client/src/components/index.component.js
+++ client/src/components/index.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './commons/nav.component';
 import Body from './commons/home.component';
-import jwtDecode from 'jwt-decode';
+// import jwtDecode from 'jwt-decode';
 
 class Index extends Component {
   render() {
