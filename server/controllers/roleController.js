0b3ba11ea857576818b4898732f9d01250b16ebc
andela-opikuda <oluwadamisi.pikuda@andela.com>
2017-05-07 05:55:49 +0100

    remove potential update and delete role functions

--- server/controllers/roleController.js
+++ server/controllers/roleController.js
@@ -38,50 +38,6 @@ class roleController {
         return response.status(200).send({ roles: roles.rows, data });
       });
   }
-
-  // /**
-  //  * Method updateRole
-  //  * @param {Object} request - request Object
-  //  * @param {Object} response - request Object
-  //  * @return {Object} response object
-  //  */
-  // static updateRole(request, response) {
-  //   model.Role.findById(request.params.id)
-  //     .then((role) => {
-  //       if (!role) {
-  //         return response.status(404)
-  //         .send({ message: `Ǹo role with id: ${request.params.role}` });
-  //       }
-
-  //       role.update(request.body)
-  //         .then((updatedRole) => {
-  //           return response.status(200)
-  //             .send(updatedRole);
-  //         });
-  //     });
-  // }
-
-  // /**
-  //  * Method deleteRole
-  //  * @param {Object} request - request Object
-  //  * @param {Object} response - request Object
-  //  * @return {Object} response object
-  //  */
-  // static deleteRole(request, response) {
-  //   model.Role.findById(request.params.id)
-  //     .then((role) => {
-  //       if (!role) {
-  //         return response.status(404)
-  //         .send({ message: `Ǹo role with id: ${request.params.role}` });
-  //       }
-
-  //       role.destroy()
-  //         .then(() => {
-  //           return response.status(200)
-  //             .send({ message: 'Succesfully deleted role' });
-  //         });
-  //     });
-  // }
 }
 
 export default roleController;
