3edc880b8d91b894adb3bb0036dedc8299bec897
andela-opikuda <oluwadamisi.pikuda@andela.com>
2017-05-08 20:19:17 +0100

    push tests

--- test/server/models/index.model.spec.js
+++ test/server/models/index.model.spec.js
@@ -1,18 +1,18 @@
-// /* eslint-disable no-unused-expressions */
-// import chai from 'chai';
-// import models from '../../../server/models';
+/* eslint-disable no-unused-expressions */
+import chai from 'chai';
+import models from '../../../server/models';
 
-// const expect = chai.expect;
+const expect = chai.expect;
 
 
-// describe('Created Models', () => {
-//   it('should have User Model Created', () => {
-//     expect(models.User).to.exist;
-//   });
-//   it('should have Role Model Created', () => {
-//     expect(models.Role).to.exist;
-//   });
-//   it('should have Document Model Created', () => {
-//     expect(models.Document).to.exist;
-//   });
-// });
+describe('Created Models', () => {
+  it('should have User Model Created', () => {
+    expect(models.User).to.exist;
+  });
+  it('should have Role Model Created', () => {
+    expect(models.Role).to.exist;
+  });
+  it('should have Document Model Created', () => {
+    expect(models.Document).to.exist;
+  });
+});
